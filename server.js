a6d77767b74bd0c1c4fdce65a7370e3d07669962
Ajay Bade <bade6281@gmail.com>
2022-12-25 19:00:02 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const dbConnect = require("./config/db/dbConnect");
+const userRoutes = require("./route/users/usersRoute");
+const { errorHandler, notFound } = require("./middlewares/error/errorHandler");
+const postRoute = require("./route/posts/postRoute");
+const commentRoutes = require("./route/comments/commentRoute");
+const emailMsgRoute = require("./route/emailMsg/emailMsgRoute");
+const categoryRoute = require("./route/category/categoryRoute");
+const cors = require('cors');
+
+const app = express();
+//DB
+dbConnect();
+
+//Middleware
+app.use(express.json());
+app.use(cors()); 
+
+//Users route
+app.use("/api/users", userRoutes);
+//Post route
+app.use("/api/posts", postRoute);
+//comment routes
+app.use("/api/comments", commentRoutes);
+//email msg
+app.use("/api/email", emailMsgRoute);
+//category route
+app.use("/api/category", categoryRoute);
+//err handler
+app.use(notFound);
+app.use(errorHandler);
+
+if(process.env.NODE_ENV="production"){
+    app.use(express.static("client/build"))
+}
+
+//server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server is running ${PORT}`));
+
+
