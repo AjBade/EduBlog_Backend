37a0e01cb9420e19d8d23f0e0315f74e548ffd41
Ajay Bade <bade6281@gmail.com>
2023-01-04 23:26:07 +0530

    change the port no to 4044

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(errorHandler);
 
 
 //server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4044;
 app.listen(PORT, console.log(`Server is running ${PORT}`));
 
 
